9ae564444fb7ebceac19a3ddaaf4ef14d487b7d5
duyenpham95 <duyenpham.june.1995@gmail.com>
2025-01-15 15:27:46 +1100

    Destructing arrays

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -3,5 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const [firstAnimal, , third] = ["giraffe", "zebra", "bear"];
+console.log(firstAnimal);
+console.log(third);
+
 createRoot(document.getElementById('root')).render(<App />
 )
